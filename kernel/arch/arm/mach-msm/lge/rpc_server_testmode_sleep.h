99f7d5a390dd4cd3f4667cc52c484c74e301b006
Julian Diaz <july491@gmail.com>
2012-10-27 00:16:48 +0200

    First commit uploaded kernel from lg open source

---
+++ kernel/arch/arm/mach-msm/lge/rpc_server_testmode_sleep.h
@@ -0,0 +1,22 @@
+/* arch/arm/mach-msm/rpc_server_testmode_motor.h
+ *
+ * Copyright (c) 2008-2009, LG Electronics. All rights reserved.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, you can find it at http://www.fsf.org.
+ */
+
+
+extern void* testmode_sleep_mode_test(uint32_t sub1_cmd, uint32_t sub2_cmd);
+
+
+
---
+++ kernel/build.sh
@@ -0,0 +1,3 @@
+export PATH=$PATH:/home/xpaum/Desktop/Android/android-toolchain-eabi/bin
+
+make ARCH=arm CROSS_COMPILE=arm-eabi-
