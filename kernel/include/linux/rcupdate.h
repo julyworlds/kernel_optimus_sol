612f5c4574c5a9a0fe7a525a03b2ff7931fa3351
Julian Diaz <july491@gmail.com>
2012-10-29 20:57:54 +0100

    Added SLQB Memory

--- kernel/include/linux/rcupdate.h
+++ kernel/include/linux/rcupdate.h
@@ -33,6 +33,7 @@
 #ifndef __LINUX_RCUPDATE_H
 #define __LINUX_RCUPDATE_H
 
+#include <linux/rcu_types.h>
 #include <linux/cache.h>
 #include <linux/spinlock.h>
 #include <linux/threads.h>
@@ -50,10 +51,13 @@ extern int rcutorture_runnable; /* for sysctl */
  * @next: next update requests in a list
  * @func: actual update function to call after the grace period.
  */
-struct rcu_head {
-	struct rcu_head *next;
-	void (*func)(struct rcu_head *head);
-};
+
+/*
+//struct rcu_head {
+//	struct rcu_head *next;
+//	void (*func)(struct rcu_head *head);
+//};
+*/
 
 /* Exported common interfaces */
 extern void rcu_barrier(void);
